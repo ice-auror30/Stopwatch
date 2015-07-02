d7d8e1ad9e256d7d4160d40e09b802f347e23f9f
svat30 <shikhar.sameer@gmail.com>
2015-07-02 13:09:50 +0530

    first commit

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,10 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    
+    var count = 0
+    
+    func result() {
+        
+        count++
+        
+        var minute = count / 60
+
+        var seconds = count % 60
+        
+        label.text = "\(minute) : \(seconds)"
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func play(sender: AnyObject) {
+    
+        if(timer.valid == false){
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        }
+        else {
+            
+        }
+
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+    
+    }
+    
+    @IBAction func Stop(sender: AnyObject) {
+    
+        timer.invalidate()
+        
+        count = 0
+        
+        label.text = "00 : 00"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+       }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
